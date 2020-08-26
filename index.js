38358b892309d8c77e40f04e7f773a16115444e5
marco-ocram <priyanshuji36@gmail.com>
2020-08-26 18:38:47 +0530

    Express REST

--- index.js
+++ index.js
@@ -1,12 +1,57 @@
 
 const express = require('express'),
-     http = require('http');
+      http = require('http');
 const morgan = require('morgan');
+
+const bodyParser = require('body-parser');
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+ 
+app.all('/dishes',(req,res,next)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/plain');
+    next();
+});
+app.get('/dishes',(req,res,next)=>{
+    res.end(`will send the dishes`);
+     
+});
+app.post('/dishes',(req,res,next)=>{
+    res.end(`Will add dish`+req.body.name+
+    `with details`+req.body.description); 
+});
+app.put('/dishes',(req,res,next)=>{
+    res.statusCode = 403;
+    res.end(`PUT NOT SUPPORTED in /dishes `); 
+});
+app.delete('/dishes',(req,res,next)=>{
+    res.end('Deleting all dishes');
+});
+
+app.get('/dishes/:dishId',(req,res,next)=>{
+    res.end('will send dish ID'
+    + req.params.dishId);
+     
+});
+
+app.post('/dishes/:dishId',(req,res,next)=>{
+    res.statusCode = 403;
+    res.end(`POST NOT SUPPORTED in /dishes//:dishes `+req.params.dishId); 
+});
+
+app.put('/dishes/:dishId',(req,res,next)=>{
+    res.write('Updating dish'+ req.params.dishId);
+    res.end('Will update dish:'+req.body.name+' with details '+req.body.description);
+
+});
+
+app.delete('/dishes/:dishId',(req,res,next)=>{
+    res.end('Deleting all dish'+req.params.dishId);
+});
 
 app.use(express.static(__dirname + "/public"));
 
@@ -17,6 +62,7 @@ app.use((req, res, next) => {
   res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 
 });
+
 
 const server = http.createServer(app);
 
